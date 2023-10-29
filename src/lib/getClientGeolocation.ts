5f23f5e51266579f7bf483398fa6973dfaacb732
Antonio Mercado <antmercado94@gmail.com>
2023-10-29 12:01:31 -0400

    Retrieve ip from req headers

--- src/lib/getClientGeolocation.ts
+++ src/lib/getClientGeolocation.ts
@@ -1,9 +1,13 @@
 import { GeoLocationData } from "@/models/GeoLocation";
 import { getGeolocation } from "@/lib/HTTP";
+import { headers } from "next/headers";
 
 export default async function getClientGeolocation() {
   try {
-    const res = await getGeolocation();
+    const headersList = headers();
+    const ip = headersList.get("x-forwarded-for") ?? "";
+
+    const res = await getGeolocation(ip);
 
     const clientGeolocation: GeoLocationData = await res.json();
 
