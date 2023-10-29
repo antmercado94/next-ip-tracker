c503f1211050eca7f6096afbee557593e784e50f
Antonio Mercado <antmercado94@gmail.com>
2023-10-29 12:13:07 -0400

    Add 'force-dynamic' route segment config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import getClientGeolocation from "@/lib/getClientGeolocation";
 import GeolocationProvider from "@/app/context/GeolocationContext";
 import Interface from "@/app/components/Interface";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   const clientGeolocation = (await getClientGeolocation()) ?? dummyData;
 
