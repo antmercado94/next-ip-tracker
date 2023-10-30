e88e8d6cf422a1090982882f380c05e72f099693
Antonio Mercado <antmercado94@gmail.com>
2023-10-30 10:52:54 -0400

    Fix server geolocation query

--- src/lib/getClientGeolocation.ts
+++ src/lib/getClientGeolocation.ts
@@ -1,13 +1,14 @@
+import { headers } from "next/headers";
 import { GeoLocationData } from "@/models/GeoLocation";
 import { getGeolocation } from "@/lib/HTTP";
-import { headers } from "next/headers";
+import { getQueryString } from "@/app/utils/getQueryString";
 
 export default async function getClientGeolocation() {
   try {
     const headersList = headers();
     const ip = headersList.get("x-forwarded-for") ?? "";
 
-    const res = await getGeolocation(ip);
+    const res = await getGeolocation(getQueryString(ip));
 
     const clientGeolocation: GeoLocationData = await res.json();
 
